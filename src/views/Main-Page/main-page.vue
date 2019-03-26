6865e816c9b5622a686142b424f2d216fea48072
João Otabe <42222777+JoaoOtabe@users.noreply.github.com>
2019-03-26 15:10:56 -0300

    Update main-page.vue

--- src/views/Main-Page/main-page.vue
+++ src/views/Main-Page/main-page.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>
-        <h1>
-            Essa é a Main Page
-        </h1>
-    </div>
+    <div class="header-main">
+  <header class = "header-estilo">
+    <section class="cabecalio-usuario">
+      
+    </section>
+    <section class="cabecalio-usu">
+      <figure>
+        <img src="../../assets/unknow_user.png" alt="foto do aluno" title="foto do aluno">
+        <figcaption>
+          <span class = "text-cabecalio">Nome do Candango</span>
+        </figcaption>
+      </figure>
+    </section>
+    <section class="cabecalio-aba">
+      <figure>
+        <img src="../../assets/unknow_aba.png" alt="foto da aba" title="foto da aba">
+        <figcaption>
+          <span class = "text-cabecalio">Nome do Candango</span>
+        </figcaption>
+      </figure>
+    </section>
+  </header>
+</div>
 </template>
 
 <script>
