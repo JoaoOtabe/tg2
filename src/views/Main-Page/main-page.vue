c59b297e0b982f610f689b4ed9ec458f2f762358
João Otabe <42222777+JoaoOtabe@users.noreply.github.com>
2019-03-26 18:19:51 -0300

    Update main-page.vue

--- src/views/Main-Page/main-page.vue
+++ src/views/Main-Page/main-page.vue
@@ -1,27 +1,19 @@
 <template>
-    <div class="header-main">
-  <header class = "header-estilo">
-    <section class="cabecalio-usuario">
-      
-    </section>
-    <section class="cabecalio-usu">
-      <figure>
-        <img src="../../assets/unknow_user.png" alt="foto do aluno" title="foto do aluno">
-        <figcaption>
-          <span class = "text-cabecalio">Nome do Candango</span>
-        </figcaption>
-      </figure>
-    </section>
-    <section class="cabecalio-aba">
-      <figure>
-        <img src="../../assets/unknow_aba.png" alt="foto da aba" title="foto da aba">
-        <figcaption>
-          <span class = "text-cabecalio">Nome do Candango</span>
-        </figcaption>
-      </figure>
-    </section>
-  </header>
-</div>
+    <div>
+        <header class="cabecalio-mainPage" role="banner">
+            <section class="cab-perfil-mainPage">
+                <img class="img-perfil-main-page" src="../../assets/default-icon.png" alt="" style="height: 30px; width: 30px;">
+                <span class="txt-nome-usuario">Nome do ser</span>
+            </section>
+            
+            <section class="cab-abas-mainPage">
+                <img class="img-aba-main-page" src="../../assets/unknow_user.png" alt="" style="height: 30px; width: 30px;">
+                <span class="txt-aba-mainPage">Nome Da Aba</span>
+            </section>
+
+            
+        </header>
+    </div>
 </template>
 
 <script>
